2ed1f22008a6c4e5ff836831d8a44c2dbf33fec5
leo murphy <leomurphylmurphy@gmail.com>
2014-11-13 21:01:39 +0200

    changes to queue

--- aws/README.md
+++ aws/README.md
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 aws
 ===
 
 AWS BOTO Scripts to connect to amazon webservices
+=======
+lab7
+====
+>>>>>>> 00f1e60baf96736fce2f8699c943b7dd3d15bfb5
--- aws/list-was-queue.py
+++ aws/list-was-queue.py
@@ -9,8 +9,24 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
-conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+conn = boto.sqs.connect_to_region("ireland-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
 
 rs = conn.get_all_queues()
 for q in rs:
 	print q.id
+
+#i think this is correct for creating a queue, this 120 represents how long
+#a message is invisable to other queues once read
+q=conn.create_queue('myqueue',120)
+
+
+#writing message
+m=message()
+m.set_body('my first message')
+q.write(m)
+
+#to remove message from queue
+q.delete_message(m)
+
+# to delete whole queue
+conn.delete_queue(q)›
